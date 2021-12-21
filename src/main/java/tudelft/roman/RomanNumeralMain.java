db08c28d60ce532c705b9e1195d71253135e951f
nana247 <emmanuel.dapaah@st.gimpa.edu.gh>
2021-12-21 21:11:38 +0100

    Create RomanNumeralMain.java

---
+++ src/main/java/tudelft/roman/RomanNumeralMain.java
@@ -0,0 +1,12 @@
+package tudelft.roman;
+
+public class RomanNumeralMain {
+    public static void main(String[] args){
+        RomanNumeral number = new RomanNumeral();
+
+        int result = number.convert("I");
+        System.out.println(result);
+
+
+    }
+}
